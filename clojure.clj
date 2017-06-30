eff1dfa2e1fec301369fbd30e742c837427cc9f4
Daniel-Hoerauf <dah4pe@virginia.edu>
2017-06-30 10:00:08 -0400

    Added filtering functionality

--- clojure.clj
+++ clojure.clj
@@ -10,14 +10,23 @@
           (fn [acc node] (conj acc (assoc node :x (count acc) :y (count %1))))
           [] %2))
       [] state))
-  (let [command-options [(repeat 10 {:action :move
+  (defn in?
+      "Return true if coll contains elem"
+      [elem coll]
+      (some #(= elem %) coll))
+  (defn filter-arena
+      "Filter the arena to return only nodes that contain one of the given type"
+      [arena & filters]
+      (let [node-list (flatten arena)]
+        (filter #(in? (get-in % [:contents :type]) filters) node-list)))
+  (let [command-options [(repeat 0 {:action :move
                                      :metadata {}})
                          (repeat 2 {:action :turn
                                     :metadata {:direction (rand-nth turn-directions)}})
-                         (repeat 4 {:action :shoot
+                         (repeat 0 {:action :shoot
                                       :metadata {}})
-                         (repeat 1 {:action :smoke
+                         (repeat 0 {:action :smoke
                                     :metadata {:direction (rand-nth smoke-directions)}})]]
 
     {:command (rand-nth (flatten command-options))
-     :state {}}))
+     :state {:state (filter-arena (add-locs (get-in state [:arena])) "fog")}}))
