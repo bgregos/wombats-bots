b3822eb41e59a48539c615c811606fe6cbf5e720
Daniel-Hoerauf <dah4pe@virginia.edu>
2017-07-13 15:30:50 -0400

    Updated bot

--- clojure.clj
+++ clojure.clj
@@ -6,10 +6,10 @@
   (defn pick-move
       "Select the move to give the highest amount of points"
       [arena self]
-      (if (can-shoot-enemy? (get-direction arena) (add-locs arena) arena-size)
+      (if (can-shoot-enemy? (get-direction arena) (add-locs arena) arena-size shot-range)
           (build-resp :shoot)
           (if (empty? (filter-arena (focus-sight arena) "food"))
-              (if (can-shoot-barrier? (get-direction arena) (add-locs arena) arena-size)
+              (if (can-shoot-barrier? (get-direction arena) (add-locs arena) arena-size shot-range)
                   (build-resp :shoot)
                   (move-to arena arena-half (get-direction arena) (select-target arena self) self))
               (move-to arena arena-half (get-direction arena) (select-target-nowall arena self) self))))
@@ -18,6 +18,6 @@
      :state {:move (pick-move (:arena state) {:x 3 :y 3})
              :saved (merge-global-state (get-global-state state :saved-state :saved) state arena-size)
              :direction (get-direction (:arena state))
-             :shootable (can-shoot-enemy? (get-direction (:arena state)) (add-locs (:arena state)) arena-size)
+             :shootable (can-shoot-enemy? (get-direction (:arena state)) (add-locs (:arena state)) arena-size shot-range)
              :distance (distance-to-tile (get-direction (:arena state)) {:x 4 :y 3})}})
              
